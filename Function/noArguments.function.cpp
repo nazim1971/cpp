feddac7ea36de7c1b9e89cd1366ca5c24264aeee
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-08 11:05:44 +0600

    no argument func

---
+++ Function/noArguments.function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Function definition
+void greet() {
+    cout << "Hello, World!" << endl;
+}
+
+int main() {
+    greet(); // Function call
+    return 0;
+}
