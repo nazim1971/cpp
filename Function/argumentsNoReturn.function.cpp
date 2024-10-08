b3ed066ace70eb105443a0c61fa4b6ed64e5fddf
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-08 11:06:06 +0600

    with argument but no return

--- Function/argumentsNoReturn.function.cpp
+++ Function/argumentsNoReturn.function.cpp
@@ -1,1 +1,13 @@
 
+#include <iostream>
+using namespace std;
+
+// Function that takes two arguments but returns nothing
+void displaySum(int a, int b) {
+    cout << "The sum is: " << (a + b) << endl;
+}
+
+int main() {
+    displaySum(5, 10); // Passing values to the function
+    return 0;
+}
