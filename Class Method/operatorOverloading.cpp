7b1550ac6a553e6cbd1d28e9aca6b7ceaea7f719
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-12 11:29:13 +0600

    add output

--- Class Method/operatorOverloading.cpp
+++ Class Method/operatorOverloading.cpp
@@ -8,3 +8,15 @@ private:
 public:
     // Constructor
     Complex(int r, int i) : real(r), imag(i) {}
+     // Overloading the + operator to add two complex numbers
+    Complex operator+(const Complex &obj) {
+        Complex result(0, 0);
+        result.real = real + obj.real;
+        result.imag = imag + obj.imag;
+        return result;
+    }
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
