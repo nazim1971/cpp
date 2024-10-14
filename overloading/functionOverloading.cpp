115db028f3dc253e1db5e475b85c8009bf75d871
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-14 13:19:42 +0600

    Pb-2 function overloading

---
+++ overloading/functionOverloading.cpp
@@ -0,0 +1,4 @@
+// Write a C++ program to demonstrate function overloading with the following functionalities:
+// Addition of two integers: The function should take two integer arguments and return their sum.
+// Multiplication of two floating-point numbers: The function should take two float arguments and return their product.
+// Subtraction of an integer from a float: The function should take a float and an int argument and return their difference (float - int).
