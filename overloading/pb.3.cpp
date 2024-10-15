caac33ed31ed7cf92c4916f7a2c508e2001e0e76
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-15 13:22:04 +0600

    Solved the pb 3

--- overloading/pb.3.cpp
+++ overloading/pb.3.cpp
@@ -1,1 +1,33 @@
 // Write a C++ program that shows the concept of an object in a function and returns an object?
+
+#include <iostream>
+using namespace std;
+
+// Class definition
+class Rectangle {
+public:
+    float length;
+    float width;
+
+    // Member function to calculate area
+    float area() {
+        return length * width;
+    }
+};
+
+// Function that takes a Rectangle object by value
+float calculateArea(Rectangle r) {
+    return r.area();
+}
+
+int main() {
+    // Create a Rectangle object and initialize values directly
+    Rectangle rect1;
+    rect1.length = 5.0;
+    rect1.width = 3.0;
+
+    // Call function to calculate area (by value)
+    cout << "Area: " << calculateArea(rect1) << endl;
+
+    return 0;
+}
