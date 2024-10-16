7c71eba315533dc19874d1146bdafc789836c1a0
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-16 13:24:50 +0600

    Explore constructor overloading

---
+++ overloading/constructorOverloading.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+public:
+    int width, height;
+
+    // Default constructor (no parameters)
+    Rectangle() {
+        width = 0;
+        height = 0;
+    }
