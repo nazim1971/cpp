36d74e18dfab45c435daac6506d49c54649abe79
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-16 13:27:55 +0600

    Done ambiguity overloading

--- overloading/ambiguityOverloading.cpp
+++ overloading/ambiguityOverloading.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+void func(int a) {
+    cout << "Integer function: " << a << endl;
+}
+
+void func(float a) {
+    cout << "Float function: " << a << endl;
+}
+
+int main() {
+    func(5.0);  // Ambiguity here
+    return 0;
+}
