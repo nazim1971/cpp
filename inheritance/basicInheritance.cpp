2f69460083eaa3500b7f7e45a02f3818fea38682
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-17 13:29:57 +0600

    Basic inheritance done

--- inheritance/basicInheritance.cpp
+++ inheritance/basicInheritance.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 using namespace std;
+
+// Base class
+class Vehicle {
+public:
+    void start() {
+        cout << "Vehicle started" << endl;
+    }
+};
+
+// Derived class with public inheritance
+class Car : public Vehicle {
+public:
+    void honk() {
+        cout << "Car horn!" << endl;
+    }
+};
+
+int main() {
+    Car myCar;
+    myCar.start(); // Inherited from Vehicle class
+    myCar.honk();  // Specific to Car class
+    return 0;
+}
