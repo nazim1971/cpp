ba86602f4623278614b7473e065d871a0fafb1a2
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-19 13:35:08 +0600

    Done hierarchical inheritance

--- inheritance/hierarchicalnheritance.cpp
+++ inheritance/hierarchicalnheritance.cpp
@@ -1,2 +1,41 @@
 #include <iostream>
 using namespace std;
+
+// Base class
+class Animal {
+public:
+    void eat() {
+        cout << "This animal eats food." << endl;
+    }
+};
+
+// Derived class 1
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "The dog barks." << endl;
+    }
+};
+
+// Derived class 2
+class Cat : public Animal {
+public:
+    void meow() {
+        cout << "The cat meows." << endl;
+    }
+};
+
+int main() {
+    Dog myDog;
+    Cat myCat;
+
+    // Dog inherits from Animal
+    myDog.eat();  // Base class method
+    myDog.bark(); // Derived class method
+
+    // Cat inherits from Animal
+    myCat.eat();  // Base class method
+    myCat.meow(); // Derived class method
+
+    return 0;
+}
