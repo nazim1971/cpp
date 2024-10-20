1f37c048a3ba003473bd827cee6a24b762ce11d8
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-20 13:38:02 +0600

    Done template

--- exception/template.cpp
+++ exception/template.cpp
@@ -1,1 +1,21 @@
 // Write a code find the max value between two number.
+#include <iostream>
+using namespace std;
+
+// Class template to find the maximum of two values
+template <typename T>
+class MaxFinder {
+public:
+    // Function to return maximum value
+    T getMax(T a, T b) {
+        return (a > b) ? a : b;
+    }
+};
+
+int main() {
+    // Using MaxFinder class for integers
+    MaxFinder<int> intMax;
+    cout << "Max of 10 and 20: " << intMax.getMax(10, 20) << endl;
+
+    return 0;
+}
