a158cb92603efbfed9b3a52ee483c5082ead6b7c
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-21 14:05:02 +0600

    Explore Exception with simple problem

---
+++ exception/exploreException.cpp
@@ -0,0 +1,1 @@
+// Write a C++ program that takes the user's name (1-3 words), country (only "Bangladesh"), and age (30 or above). Use exception handling to validate the inputs and throw appropriate error messages if any condition is violated.
