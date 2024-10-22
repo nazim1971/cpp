d0140ebe1b83c3b39179f3f1296ee6757cdb4c68
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-22 13:11:39 +0600

    work on pb-1

--- Lab/pb-1.cpp
+++ Lab/pb-1.cpp
@@ -1,1 +1,12 @@
 // Problem-1:  Write a C++ program to demonstrate single inheritance where the base class has a method showBase() and the derived class adds another method showDerived().
+
+#include <iostream>
+using namespace std;
+
+// Base class
+class Base {
+public:
+    void showBase() {
+        cout << "This is the base class." << endl;
+    }
+};
