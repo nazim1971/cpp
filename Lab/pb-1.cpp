1010adba48c12e0d2c1cdf76a4024da63baf02f4
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-22 13:12:04 +0600

    Solved the pb-1

--- Lab/pb-1.cpp
+++ Lab/pb-1.cpp
@@ -10,3 +10,22 @@ public:
         cout << "This is the base class." << endl;
     }
 };
+
+// Derived class inheriting from Base
+class Derived: public Base {
+public:
+    void showDerived() {
+        cout << "This is the derived class." << endl;
+    }
+};
+
+int main() {
+    // Create an object of the derived class
+    Derived obj;
+    // Call the base class method using the derived class object
+    obj.showBase();
+    // Call the derived class method
+    obj.showDerived();
+
+    return 0;
+}
