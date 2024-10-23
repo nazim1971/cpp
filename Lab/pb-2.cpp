7421d9fbadcea6219ebd8988ab92d265d13130bc
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-23 13:16:43 +0600

    work on 2

--- Lab/pb-2.cpp
+++ Lab/pb-2.cpp
@@ -1,2 +1,11 @@
 // Problem 2: Create a program with a base class that has protected and private members. Show how the derived class can access the protected member but not the private member
 
+#include <iostream>
+using namespace std;
+
+class Base {
+protected:
+    int protectedMember = 10;
+private:
+    int privateMember = 20;
+};
