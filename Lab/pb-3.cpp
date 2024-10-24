7206ffbca06dcd7dcb857f5c39c59ab592c5540c
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-24 13:22:12 +0600

    add pb-3

--- Lab/pb-3.cpp
+++ Lab/pb-3.cpp
@@ -1,2 +1,13 @@
 // Problem -3: Write a program to demonstrate the use of virtual functions in a base class and how they are overridden in a derived class
 
+#include <iostream>
+using namespace std;
+
+// Base class
+class Base {
+public:
+    // Virtual function
+    virtual void show() {
+        cout << "Base class method." << endl;
+    }
+};
