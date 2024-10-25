f147732bf8933a77a73b9566ecb6a8b7d41f136b
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-25 13:24:07 +0600

    woprk on  pb-4

--- Lab/pb-4.cpp
+++ Lab/pb-4.cpp
@@ -1,1 +1,12 @@
 // Problem-4: Create a program that uses multiple inheritance, where two base classes have methods with the same name, and demonstrate how ambiguity can be resolved.
+
+#include <iostream>
+using namespace std;
+
+// Base class 1
+class Base1 {
+public:
+    void show() {
+        cout << "Base1 class method." << endl;
+    }
+};
