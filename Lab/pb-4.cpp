e93ae46df74a4fb5d529e1cee39c2c70958b8ba9
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-25 13:24:32 +0600

    solved pb-4

--- Lab/pb-4.cpp
+++ Lab/pb-4.cpp
@@ -10,3 +10,31 @@ public:
         cout << "Base1 class method." << endl;
     }
 };
+
+// Base class 2
+class Base2 {
+public:
+    void show() {
+        cout << "Base2 class method." << endl;
+    }
+};
+
+// Derived class inheriting from both Base1 and Base2
+class Derived : public Base1, public Base2 {
+public:
+    // Resolving ambiguity by specifying which base class method to call
+    void showBase() {
+        Base1::show(); // Call Base1's show method
+        Base2::show(); // Call Base2's show method
+    }
+};
+
+int main() {
+    // Create an object of Derived class
+    Derived obj;
+
+    // Call the methods from both base classes explicitly
+    obj.showBase();  // Calls Base1's and Base2's show()
+    
+    return 0;
+}
