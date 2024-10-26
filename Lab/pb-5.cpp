af9a12269a0381b4d7f1bcd24b18e95203dc0bb8
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-26 13:27:08 +0600

    work on pb-5

--- Lab/pb-5.cpp
+++ Lab/pb-5.cpp
@@ -1,1 +1,10 @@
 // Problem-5 : Write a function template to calculate the maximum of two numbers of any type (int, float, etc.).
+
+#include <iostream>
+using namespace std;
+
+// Function template to calculate the maximum of two numbers
+template <typename T>
+T findMax(T a, T b) {
+    return (a > b) ? a : b;
+}
