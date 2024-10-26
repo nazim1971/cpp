bf71b52f3d21433c999d9edb62c7c895d579e54b
nazim1971 <md.nazimuddinaj@gmail.com>
2024-10-26 13:27:26 +0600

    Solved pb-5

--- Lab/pb-5.cpp
+++ Lab/pb-5.cpp
@@ -8,3 +8,9 @@ template <typename T>
 T findMax(T a, T b) {
     return (a > b) ? a : b;
 }
+
+int main() {
+    cout << "Max of 3 and 5: " << findMax(3, 5) << endl;
+    cout << "Max of 3.5 and 2.7: " << findMax(3.5, 2.7) << endl;
+    return 0;
+}
